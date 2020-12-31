43cdd31aedf048ddafff11750280f810534f7d4b
izark <izarknguyen@gmail.com>
2020-12-31 23:34:53 +0700

    fix movie UI

--- lib/views/ui/movie_ui.dart
+++ lib/views/ui/movie_ui.dart
@@ -17,46 +17,61 @@ class MovieUI extends StatelessWidget {
           init: DataController(),
           builder: (_) {
             if (_.detailName.length > 0) {
-              return Column(
-                children: [
-                  MovieTitle(detailName: _.detailName),
-                  _.detailChapterTitleList.length > 0
-                      ? Flexible(
-                          child: Player(),
-                          fit: FlexFit.tight,
-                        )
-                      : MovieImage(detailImage: _.detailImage),
-                  SIZED_BOX_H10,
-                  Center(
-                    child: Text(
+              return OrientationBuilder(
+                builder: (BuildContext context, Orientation orientation) {
+                  return Column(
+                    children: [
+                      Visibility(
+                        visible: orientation == Orientation.portrait,
+                        child: MovieTitle(detailName: _.detailName),
+                      ),
                       _.detailChapterTitleList.length > 0
-                          ? STR_LIST_CHAPTER
-                          : STR_LIST_CHAPTER_EMPTY,
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                  ),
-                  Visibility(
-                    visible: _.detailChapterTitleList.length > 0,
-                    child: Expanded(
-                      child: MovieChapterList(
-                        detailChapterLinkList: _.detailChapterLinkList,
-                        detailChapterTitleList: _.detailChapterTitleList,
+                          ? Player()
+                          : MovieImage(detailImage: _.detailImage),
+                      Visibility(
+                        visible: orientation == Orientation.portrait,
+                        child: SIZED_BOX_H10,
                       ),
-                    ),
-                  ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        STR_SHOW_TIME + _.detailShowtime.trim().toUpperCase(),
-                        textAlign: TextAlign.center,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.bodyText2,
+                      Visibility(
+                        visible: orientation == Orientation.portrait,
+                        child: Center(
+                          child: Text(
+                            _.detailChapterTitleList.length > 0
+                                ? STR_LIST_CHAPTER
+                                : STR_LIST_CHAPTER_EMPTY,
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                ],
+                      Visibility(
+                        visible: _.detailChapterTitleList.length > 0 &&
+                            orientation == Orientation.portrait,
+                        child: Expanded(
+                          child: MovieChapterList(
+                            detailChapterLinkList: _.detailChapterLinkList,
+                            detailChapterTitleList: _.detailChapterTitleList,
+                          ),
+                        ),
+                      ),
+                      Visibility(
+                        visible: orientation == Orientation.portrait,
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              STR_SHOW_TIME +
+                                  _.detailShowtime.trim().toUpperCase(),
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
               );
             } else {
               return Loading();
