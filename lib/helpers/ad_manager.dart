da5b512b9bb9237ffab14847139dbfa8e47d8bdb
izark <izarknguyen@gmail.com>
2021-01-01 20:43:15 +0700

    1.0.5

---
+++ lib/helpers/ad_manager.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_admob/firebase_admob.dart';
+
+class AdManager {
+  static InterstitialAd _interstitialAd;
+
+  String getAdMobAppId() {
+    return "ca-app-pub-9658728883249380~8077339954";
+  }
+
+  String getInterstitialAdId() {
+    // return "ca-app-pub-9658728883249380/1511931606";
+    return "ca-app-pub-3940256099942544/1033173712";
+  }
+
+  InterstitialAd getInterstitialAd() {
+    return InterstitialAd(
+      adUnitId: getInterstitialAdId(),
+      listener: (MobileAdEvent event) {
+        print(event);
+      },
+    );
+  }
+
+  void showIntertitialAd() {
+    if (_interstitialAd == null) _interstitialAd = getInterstitialAd();
+    _interstitialAd
+      ..load()
+      ..show(
+        anchorType: AnchorType.bottom,
+        anchorOffset: 0.0,
+        horizontalCenterOffset: 0.0,
+      );
+  }
+}
